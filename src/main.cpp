fd544e80bb234502df549100868071824f71ae8c
Thakkar Sarjak <sarjak.thakkar@nokia.com>
2019-11-17 17:54:11 -0500

    Add 3d options

--- src/main.cpp
+++ src/main.cpp
@@ -127,6 +127,19 @@ int main(void){
 
   glfwMakeContextCurrent(window);
 
+  // Enable some options for 3D
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
+  glEnable(GL_BLEND);
+
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glCullFace(GL_BACK);
+  glFrontFace(GL_CCW);
+
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+
   // Initialize GLEW
   #ifndef __APPLE__
     glewExperimental = true;
